4529a81c4a2e0321669169c13f11576a37f746b9
Michael Cheng <michael_cheng@mom.gov.sg>
2023-01-14 18:40:14 +0800

    R8 - Debatable change
    - Use of 1 function to appreciate / depreciate

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -19,74 +19,56 @@ class Shop {
     return item.sellIn < 0
   }
 
-  appreciatedQuality(item, rate=this.rateOfChange) {
-    if (item.quality != this.maxQuality) {
-      item.quality = item.quality + rate
+  updateItemQuality(direction, item, rate) {
+    switch (direction) {
+      case 'APPRECIATE':
+        item.quality = item.quality + rate
+        break;
+      case 'DEPRECIATE':
+        item.quality = item.quality - rate
+        break;
+      default:
+        break;
     }
+
     if (item.quality > 50) item.quality = 50
-
-    return item
-  }
-
-  depreciateQuality(item, rate=this.rateOfChange) {
-    if (item.quality > 0) {
-      item.quality = item.quality - rate
-    }
     if (item.quality < 0) item.quality = 0
 
     return item
   }
 
   updateItem(item) {
-    let rateOfQualityChange = this.rateOfChange 
-
     if (this.legendaryItems.includes(item.name)) {
       return item
     }
 
     item.sellIn = item.sellIn - 1
+    
+    let rateOfQualityChange = this.rateOfChange
+    let qualityChangeType = this.appreciatingItems.includes(item.name) ? 'APPRECIATE' : 'DEPRECIATE'
 
     if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.isExpired(item)) {
+        item.quality = 0
+        return item
+      }
+      
       if (item.sellIn < 5) {
         rateOfQualityChange = rateOfQualityChange * 3
       } else if (item.sellIn < 10) {
         rateOfQualityChange = rateOfQualityChange * 2
       }
-
-      item = this.appreciatedQuality(item, rateOfQualityChange)
-
-      if (this.isExpired(item)) item.quality = 0
-
-      return item
-    }
-
-    if (item.name === 'Aged Brie' ) {
-      if (this.isExpired(item)) {
-        rateOfQualityChange = rateOfQualityChange * 2
-      }
-
-      item = this.appreciatedQuality(item, rateOfQualityChange)
-
-      return item
     }
 
     if (item.name === 'Conjured' ) {
       rateOfQualityChange = rateOfQualityChange * 2
-
-      if (this.isExpired(item)) {
-        rateOfQualityChange = rateOfQualityChange * 2
-      }
-
-      item = this.depreciateQuality(item, rateOfQualityChange)
-
-      return item
     }
 
     if (this.isExpired(item)) {
       rateOfQualityChange = rateOfQualityChange * 2
     } 
 
-    item = this.depreciateQuality(item, rateOfQualityChange)
+    item = this.updateItemQuality(qualityChangeType, item, rateOfQualityChange)
 
     return item
   }
