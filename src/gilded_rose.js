5ac610285b43fd6f14b941a59e9cd3f960b4c538
Michael Cheng <michael_cheng@mom.gov.sg>
2023-01-14 17:44:54 +0800

    R5

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,12 +14,25 @@ class Shop {
     this.maxQuality = 50
   }
 
-  isMaxQuality(item) {
-    return item.quality === this.maxQuality
-  }
-
   isExpired(item) {
     return item.sellIn < 0
+  }
+
+  appreciatedQuality(item, rate=1) {
+    if (item.quality != this.maxQuality) {
+      item.quality = item.quality + rate
+    }
+    if (item.quality > 50) item.quality = 50
+
+    return item
+  }
+
+  depreciateQuality(item, rate=1) {
+    if (item.quality > 0) {
+      item.quality = item.quality - rate
+    }
+
+    return item
   }
 
   updateItem(item) {
@@ -30,20 +43,16 @@ class Shop {
     item.sellIn = item.sellIn - 1
 
     if (this.appreciatingItems.includes(item.name)) {
-      if (!this.isMaxQuality(item)) {
-        item.quality = item.quality + 1
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 10) {
-            if (!this.isMaxQuality(item)) {
-              item.quality = item.quality + 1
-            }
-          }
-          if (item.sellIn < 5) {
-            if (!this.isMaxQuality(item)) {
-              item.quality = item.quality + 1
-            }
-          }
+      if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.sellIn < 5) {
+          item = this.appreciatedQuality(item, 3)
+        } else if (item.sellIn < 10) {
+          item = this.appreciatedQuality(item, 2)
+        } else {
+          item = this.appreciatedQuality(item)
         }
+      } else {
+        item = this.appreciatedQuality(item)
       }
     } else {
       if (item.quality > 0) {
@@ -53,17 +62,11 @@ class Shop {
 
     if (this.isExpired(item)) {
       if (item.name === 'Aged Brie') {
-        if (!this.isMaxQuality(item)) {
-          item.quality = item.quality + 1
-        }
+        item = this.appreciatedQuality(item)
+      } else if(item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        item.quality = 0
       } else {
-        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1
-          }
-        } else {
-          item.quality = item.quality - item.quality
-        }
+        item = this.depreciateQuality(item)
       }
     }
 
